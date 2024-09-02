dbc56fed31f21a4b42023b5aa40cffe220525a6b
Алексей Никитин <rysis17@yandex.ru>
2024-09-02 22:33:52 +0300

    add stage 8

---
+++ bin/brain-progression.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import { getGame, taskGame, startGame } from '../src/games/progression.js';
+
+getGame(taskGame, startGame);
---
+++ src/games/progression.js
@@ -0,0 +1,27 @@
+import { getRandomNumber } from '../utils.js';
+import getGame from '../index.js';
+
+const taskGame = 'What number is missing in the progression?';
+
+const getProgression = (number, interval) => {
+  const numbers = [];
+  let member = number;
+  while (numbers.length <= 10) {
+    member += interval;
+    numbers.push(member);
+  }
+  return numbers;
+};
+
+const startGame = () => {
+  const firstNumber = getRandomNumber(0, 20);
+  const interval = getRandomNumber(2, 10);
+  const index = getRandomNumber(0, 9);
+  const progression = getProgression(firstNumber, interval);
+  const answer = progression[index].toString();
+  progression[index] = '..';
+  const question = progression.join(' ');
+  return [answer, question];
+};
+
+export { getGame, taskGame, startGame };
