09abc68b6a85ac7233666a629bd691f910f0de25
Individ <114526066+individ@users.noreply.github.com>
2024-09-15 22:24:45 +0300

    Update prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -5,12 +5,12 @@ const taskGame = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 const isPrime = (number) => {
   if (number <= 1) {
-    return false; // Числа 0 и 1 не являются простыми
+    return false;
   }
 
   for (let i = 2; i <= Math.sqrt(number); i += 1) {
     if (number % i === 0) {
-      return false; // Если число делится на i, оно составное
+      return false;
     }
   }
 
